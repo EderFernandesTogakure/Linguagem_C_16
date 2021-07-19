75fc097c8a2549ca8d1bfe76c73d66bad5fdd9ec
Eder Carlos Fernandes <eder.tecnologias@gmail.com>
2021-07-19 20:31:13 -0300

    Add files via upload

---
+++ aula16_1/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int contador;
+
+    for(contador = 1; contador <=10; contador ++){
+        printf("Contando %d !\n", contador);
+    }
+
+}
